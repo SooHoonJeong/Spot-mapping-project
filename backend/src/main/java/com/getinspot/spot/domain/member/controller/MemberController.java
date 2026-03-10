4bd7ed6ae1a8c538616cd8c7e7781675b2f32bd0
YongJun Cho <whdydwns73@naver.com>
2026-03-10 21:59:10 +0900

    comment: MemberController 코드와 맞지 않는 설명 주석 제거

--- backend/src/main/java/com/getinspot/spot/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/getinspot/spot/domain/member/controller/MemberController.java
@@ -17,10 +17,8 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    // 내 프로필 조회 (네이버 스타일 UI용 데이터 응답)
     @GetMapping("/me")
     public ResponseEntity<MemberProfileResponse> getMyProfile(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        // 토큰에서 추출한 id를 사용해 서비스 호출
         MemberProfileResponse response = memberService.getMyProfile(userDetails.getId());
         return ResponseEntity.ok(response);
     }
